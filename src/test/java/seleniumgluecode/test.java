a61426efb6cae03ff0c79ed4b1c1084f7cdfaa20
Chris Davies <Chris@CuriosityKilledThe.Cat>
2019-02-13 22:34:12 +1100

    Update an order

--- src/test/java/seleniumgluecode/test.java
+++ src/test/java/seleniumgluecode/test.java
@@ -43,11 +43,8 @@ public class test {
 	
 	@Given("^the user is logged in$")
 	public void the_user_is_logged_in() throws Throwable {
-		System.out.println("DEBUG POINT A");
 		the_user_is_on_the_login_screen();
-		System.out.println("DEBUG POINT B");
 		the_user_enters_their_username_and_password();
-		System.out.println("DEBUG POINT C");
 		clicks_the_login_button();
 	}
 	
@@ -64,8 +61,6 @@ public class test {
 		String PageTitle = driver.getTitle();
         Assert.assertEquals("Web Orders Login", PageTitle);
 	}	
-	
-	
 	
 	@Given("^the user has loaded the order page$")
 	public void the_user_has_loaded_the_order_page() throws Throwable {
@@ -99,4 +94,50 @@ public class test {
 		//System.out.println("Page text is: " + PageText);
 		Assert.assertThat(PageText, CoreMatchers.containsString("New order has been successfully added."));	  
 	}
+		
+	@Given("^the user has placed an order$")
+	public void the_user_has_placed_an_order() throws Throwable {
+	    // login
+		the_user_is_on_the_login_screen();
+		the_user_enters_their_username_and_password();
+		clicks_the_login_button();
+		the_user_has_loaded_the_order_page();
+		
+		// place an order
+		the_user_has_loaded_the_order_page();
+		the_user_has_completed_the_order_form();
+		the_user_clicks_process();
+		the_order_will_be_added_to_the_system();
+	}
+	
+	@When("^the user switches to the view orders form$")
+	public void the_user_switches_to_the_view_orders_form() throws Throwable {
+	    // View all orders
+		driver.findElement(By.linkText("View all orders")).click();
+	}
+	
+	
+	@When("^the user updates the order using the form$")
+	public void the_user_updates_the_order_using_the_form() throws Throwable {
+	    // Click the edit icon (assume they want to edit the first order in the list):
+		driver.findElement(By.cssSelector("#ctl00_MainContent_orderGrid > tbody > tr:nth-child(2) > td:nth-child(13) > input[type=\"image\"]")).click();   
+		Thread.sleep(2000);
+		
+		//Update the post/zip code:
+		driver.findElement(By.id("ctl00_MainContent_fmwOrder_TextBox5")).clear();
+		driver.findElement(By.id("ctl00_MainContent_fmwOrder_TextBox5")).sendKeys("3210");
+		driver.findElement(By.id("ctl00_MainContent_fmwOrder_UpdateButton")).click();
+		}
+	
+	@Then("^the order is updated$")
+	public void the_order_is_updated() throws Throwable {
+	    // Check the list all orders page is shown
+		
+	}
+	
+	
+	
+
+	
+
 }
